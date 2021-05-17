f759de5c558030b679130e17a9809c80a882722c
ssds_ <dorosh.kostya65@gmail.com>
2021-05-17 11:13:09 +0300

    Prepare for deploy

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const config = require('config')
 const db = require('./models')
+const path = require('path')
+
 const app = express()
 
 app.use(express.json({extended: true}))
@@ -13,6 +15,14 @@ app.use('/', require('./routes/userProfile'))
 app.use('/', require('./routes/deleteAccount'))
 app.use('/', require('./routes/editProfile'))
 app.use('/', require('./routes/companyPage'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || config.get('port') || 5000
 
